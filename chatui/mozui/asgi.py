490c3f23ff2eeed3de204b1e88237af2205eb832
delphine nyaboke <delphinenyaboke@gmail.com>
2023-04-26 13:53:49 +0300

    relevant files

--- chatui/mozui/asgi.py
+++ chatui/mozui/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for chatui project.
+ASGI config for mozui project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatui.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mozui.settings')
 
 application = get_asgi_application()
