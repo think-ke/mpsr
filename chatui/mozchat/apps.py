05b42cc5f7fa164e148d4b41437d77b19419b000
delphine nyaboke <delphinenyaboke@gmail.com>
2023-04-26 12:57:44 +0300

    begin (again) the chatui process

--- chatui/mozchat/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MozchatConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "mozchat"
--- chatui/mozchat/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-import requests
-import json
-
-# Create your views here.
-
-def home(request):
-    return render(request, template_name='index.html')
